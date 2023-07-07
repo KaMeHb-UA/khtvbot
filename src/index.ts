b1c5da910722525600b32fd05eacba08076a0077
KaMeHb-UA <blade.whiteblack@gmail.com>
2023-07-07 15:31:43 +0300

    add greeting removing logic

--- src/index.ts
+++ src/index.ts
@@ -2,13 +2,14 @@ import init from './init';
 import bot from './bot';
 
 export default {
-	async fetch(request: Request, env: Record<string, string>) {
+	async fetch(request: Request, env: Record<string, string>, context: any) {
 		init(env);
 		try {
-			await bot.processUpdate(
+			const { waitUntil } = await bot.processUpdate(
 				request.headers.get('X-Telegram-Bot-Api-Secret-Token'),
 				await request.json(),
 			);
+			context.waitUntil(waitUntil);
 			return new Response('OK');
 		} catch(e) {
 			console.error(e);
