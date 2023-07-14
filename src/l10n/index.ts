f709b0077d62a3c7dd120560a50d846bdc0f3579
KaMeHb-UA <blade.whiteblack@gmail.com>
2023-07-14 05:51:14 +0300

    update admin translations

--- src/l10n/index.ts
+++ src/l10n/index.ts
@@ -12,15 +12,20 @@ export type Phrases = {
 		groupName: string;
 		groupRulesLink: string;
 	};
-	welcome_admin: {
+	admin_dashboard: {
 		userName: string;
 		groupLink: string;
 		groupName: string;
 	};
+	admin_ban_cmd: Record<string, never>;
+	admin_mute_cmd: Record<string, never>;
+	admin_back_cmd: Record<string, never>;
+	admin_ban_view: Record<string, never>;
+	admin_mute_view: Record<string, never>;
 };
 
 export default function translate<L extends keyof typeof languages, P extends keyof Phrases>(language: L, phrase: P, variables: Phrases[P]) {
-	let text: string = languages[language]?.[phrase] || `Can't find phrase "${phrase}" in ${language} language. Please ensure that language & phrase are correct and linked to translation plugin`;
+	let text: string = languages[language]?.[phrase]?.trim?.() || `Can't find phrase "${phrase}" in ${language} language. Please ensure that language & phrase are correct and linked to translation plugin`;
 	for (const [name, value] of Object.entries(variables)) {
 		text = text.replaceAll('${' + name + '}', String(value));
 	}
