b3ccbc77f0fe474d62bf1da0679522e645edde0f
KaMeHb-UA <blade.whiteblack@gmail.com>
2023-07-14 05:55:58 +0300

    add (de-)serialization for integers

--- src/helpers/serialization.ts
+++ src/helpers/serialization.ts
@@ -9,3 +9,21 @@ export function fixChatId(chatId: number) {
 	}
 	return chatId;
 }
+
+export function intToBase64(int: number | bigint) {
+	const hex = int.toString(16);
+	const bytes = [];
+	for (let c = 0; c < hex.length; c += 2) {
+		bytes.push(parseInt(hex.slice(c, c + 2), 16));
+	}
+	return btoa(String.fromCharCode(...new Uint8Array(bytes)));
+}
+
+export function base64ToInt(base64: string) {
+	const binaryString = atob(base64);
+	const bytes = [];
+	for (let i = 0; i < binaryString.length; i++) {
+		bytes.push(binaryString.charCodeAt(i).toString(16).padStart(2, '0'));
+	}
+	return BigInt('0x' + bytes.join(''));
+}
