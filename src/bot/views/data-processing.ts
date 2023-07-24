2ee3750249e4f86b91f1175ebf43ab659bd18228
KaMeHb-UA <blade.whiteblack@gmail.com>
2023-07-24 05:01:22 +0300

    add basic user search functionality

---
+++ src/bot/views/data-processing.ts
@@ -0,0 +1,10 @@
+import type { View } from '.';
+
+export const id = 0x05;
+
+export default (): View => {
+	return {
+		text: 'admin_data_processing_message',
+		buttons: [],
+	};
+};
--- src/bot/views/index.ts
+++ src/bot/views/index.ts
@@ -3,11 +3,17 @@ import type { TGButton } from '../markup';
 import * as dashboard from './dashboard';
 import * as ban from './ban';
 import * as mute from './mute';
+import * as search from './search';
+import * as  searchPrompt from './search-prompt';
+import * as  dataProcessing from './data-processing';
 
 const viewList = [
 	dashboard,
 	ban,
 	mute,
+	search,
+	searchPrompt,
+	dataProcessing,
 ];
 
 type AdminPhrases<T extends string> = T extends `admin_${infer _}` ? T : never;
@@ -19,6 +25,9 @@ export type View = {
 
 export type ViewArgs = {
 	groupId: number;
+	searchText?: string;
+	opcodeSequence?: string;
+	initialArgs?: string[];
 };
 
 const views: {
