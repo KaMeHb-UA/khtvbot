04bf16376253979818a06836d3389d08b01a1dfd
KaMeHb-UA <blade.whiteblack@gmail.com>
2023-07-26 07:07:52 +0300

    use `ntfy.sh` to notify about errors in system

--- src/init.ts
+++ src/init.ts
@@ -1,8 +1,10 @@
 import type { Environment } from './platform';
 import bot from './bot';
 import DB from './db';
+import ntfy from './ntfy';
 
-export default function init({ BOT_TOKEN, BOT_WEBHOOK_SECRET, SUPABASE_URL, SUPABASE_KEY }: Environment) {
+export default function init({ BOT_TOKEN, BOT_WEBHOOK_SECRET, SUPABASE_URL, SUPABASE_KEY, NTFY_TOPIC }: Environment) {
 	bot.init(BOT_TOKEN, BOT_WEBHOOK_SECRET);
 	DB.init(SUPABASE_URL, SUPABASE_KEY);
+	ntfy.init(NTFY_TOPIC);
 }
